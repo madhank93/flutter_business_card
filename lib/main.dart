7d27662a8b9eb939c7e92be80fc7d1045edc5812
madhank93 <maddymadhan946@gmail.com>
2020-04-12 19:52:51 +0530

    adding fonts and images

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,56 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
+      home: MyApp(),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundImage: AssetImage("assets/images/male-avatar.png"),
+            ),
+            Text(
+              "Madhan",
+              style: TextStyle(
+                fontFamily: "Kalam",
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              "SDET and Flutter Enthusiats",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                letterSpacing: 1.0,
+              ),
+            ),
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    size: 10,
+                  ),
+                  Text("+91 12345 12345", style: TextStyle(
+                    fontSize: 20
+                  ),),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
