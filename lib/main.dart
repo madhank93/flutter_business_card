41dc4ebcb0e1b217e33d2f5e38c46663ea9ef6c4
madhank93 <maddymadhan946@gmail.com>
2020-04-12 20:54:08 +0530

    completion

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MyApp(),
     ),
   );
@@ -18,10 +19,11 @@ class MyApp extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CircleAvatar(
+              radius: 50.0,
               backgroundImage: AssetImage("assets/images/male-avatar.png"),
             ),
             Text(
-              "Madhan",
+              "Madhan K",
               style: TextStyle(
                 fontFamily: "Kalam",
                 fontSize: 40.0,
@@ -34,22 +36,53 @@ class MyApp extends StatelessWidget {
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 20,
+                color: Colors.white,
                 letterSpacing: 1.0,
               ),
             ),
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.phone,
-                    size: 10,
-                  ),
-                  Text("+91 12345 12345", style: TextStyle(
-                    fontSize: 20
-                  ),),
-                ],
+            SizedBox(
+              width: 200.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+                thickness: 0.4,
               ),
-            )
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  size: 20,
+                  color: Colors.teal.shade900,
+                ),
+                title: Text(
+                  "+91 12345 12345",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.teal.shade900,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  size: 20,
+                  color: Colors.teal.shade900,
+                ),
+                title: Text(
+                  "abc@mail.com",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.teal.shade900,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
